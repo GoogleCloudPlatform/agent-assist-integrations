59bdf02e8a81bfba9a9ff02b07121de819c8e27f
Nate Donaldson <nategd@google.com>
2025-10-07 20:38:35 +0000

    Correct deploy script

--- aa-integration-backend/deploy.sh
+++ aa-integration-backend/deploy.sh
@@ -284,7 +284,7 @@ if [[ -z $VPC_CONNECTOR_NAME ]]; then
   --subnet $VPC_SUBNET \
   --clear-vpc-connector \
   --min-instances=1 \
-  --max-instance-request-concurrency=80 \
+  --concurrency=80 \
   --no-cpu-throttling \
   --update-secrets=/secret/jwt_secret_key=${JWT_SECRET_NAME}:latest \
   --set-env-vars REDISHOST=$REDIS_HOST \
@@ -338,8 +338,8 @@ if [[ -z $VPC_CONNECTOR_NAME ]]; then
     --subnet $VPC_SUBNET \
     --clear-vpc-connector \
     --ingress=internal \
-    --max-instance-request-concurrency=80 \
     --min-instances=1 \
+    --concurrency=80 \
     --no-cpu-throttling \
     --set-env-vars REDISHOST=$REDIS_HOST \
     --set-env-vars REDISPORT=$REDIS_PORT
